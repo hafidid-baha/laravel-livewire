dcf4ada013e2b1b27d9bef9fcad0ff20c9f5ec1a
Evel25 <bi3.h300@gmail.com>
2020-06-20 12:49:56 +0100

    update livewire search component

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Livewire;
 
+use App\Places;
 use Livewire\Component;
 
 class Search extends Component
 {
 
     public $search = '';
+    public $data;
 
     public function render()
     {
         return view('livewire.search');
     }
+
+    
+
 }
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,7 @@
 <div>
-    <input class="form-control mt-4 w-50 mx-auto" type="text" placeholder="Default input">
+    <input class="form-control mt-4 w-50 mx-auto" wire:keydown="getData()" wire:model="search" type="text" placeholder="Search For Place">
+    <p>{{$search}}</p>
+    <ul class="list-group w-50 mt-4 mx-auto">
+        <li class="list-group-item">Cras justo odio</li>
+    </ul>
 </div>
