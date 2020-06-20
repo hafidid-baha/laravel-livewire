4b9874cea27170381cd7c113f6f9a8448b58396e
Evel25 <bi3.h300@gmail.com>
2020-06-20 11:38:29 +0100

    create the PlaceFactory Class

---
+++ database/factories/PlacesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Places;
+use Faker\Generator as Faker;
+
+$factory->define(Places::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
