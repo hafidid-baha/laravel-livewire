259ded08a3601cb10198b3bb5dc4e06173efc2d7
Evel25 <bi3.h300@gmail.com>
2020-06-20 11:08:41 +0100

    create the counter component

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <button class="btn btn-danger" wire:click="increment">+</button>
+    <span class="d-inline-block">{{ $count }}</span>
+    <button class="btn btn-danger" wire:click="decrement">-</button>
+</div>
