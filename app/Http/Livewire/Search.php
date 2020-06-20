a5a7347a772808679d8d85a730b0e70e6c100cf1
Evel25 <bi3.h300@gmail.com>
2020-06-20 14:40:27 +0100

    update livewire component file upgrading the search method

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -8,31 +8,39 @@ use Livewire\Component;
 class Search extends Component
 {
 
-    public $search = '';
-    public $data= array();
-    public $newData= array();
+    public $search;
+    // public $data= array();
+    // public $newData= array();
+    protected $updatesQueryString = ['search'];
+
+    public function mount(){
+        $this->search = request()->query('search',$this->search);
+    }
 
     public function render()
     {
-        $this->data = Places::all()->toArray();
-        return view('livewire.search');
+
+        // $this->data = Places::all()->toArray();
+        return view('livewire.search',[
+            'items'=>Places::where('name','like','%'.$this->search.'%')->get()
+        ]);
     }
 
-    public function getData(){
-        // $s = '%'.$this->search.'%';
-        $s = $this->search;
-        // $this->data = Places::where('name','like','kjhkj')->toSql();
-        // $this->data = Places::where('name','like','%'.$this->search.'%')->get();
-        // $this->data = Places::all();
-        if($s != ''){
-            $this->newData = array_filter($this->data,function($item)use($s){
-                // var_dump($item['name'],$s);
-                return strpos($item['name'], $s);
-            });
-        }else{
-            $this->newData = array();
-        }
+    // public function getData(){
+    //     // $s = '%'.$this->search.'%';
+    //     $s = $this->search;
+    //     // $this->data = Places::where('name','like','kjhkj')->toSql();
+    //     // $this->data = Places::where('name','like','%'.$this->search.'%')->get();
+    //     // $this->data = Places::all();
+    //     if($s != ''){
+    //         $this->newData = array_filter($this->data,function($item)use($s){
+    //             // var_dump($item['name'],$s);
+    //             return strpos($item['name'], $s);
+    //         });
+    //     }else{
+    //         $this->newData = array();
+    //     }
         
-    }
+    // }
 
 }
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,9 +1,9 @@
 <div>
-    <input class="form-control mt-4 w-50 mx-auto" wire:keydown="getData()" wire:model="search" type="text" placeholder="Search For Place">
+    <input class="form-control mt-4 w-50 mx-auto" wire:model="search" type="text" placeholder="Search For Place">
     
     <ul class="list-group w-50 mt-4 mx-auto">
-        @foreach ($newData as $d)
-            <li class="list-group-item">{{$d['name']}}</li>
+        @foreach ($items as $item)
+            <li class="list-group-item">{{$item->name}}</li>
         @endforeach
         
     </ul>
